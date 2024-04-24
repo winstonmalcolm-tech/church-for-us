eb548dabdee4db45ac1499267eea11c402cfee59
Winston Malcolm <programmermalx27@gmail.com>
2024-04-24 06:07:27 -0500

    added animation

--- lib/routes/auth.dart
+++ lib/routes/auth.dart
@@ -1,5 +1,9 @@
+import "dart:async";
+
 import "package:email_validator/email_validator.dart";
+import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
+import "package:flutter/scheduler.dart";
 import "package:flutter/widgets.dart";
 
 class Authentication extends StatefulWidget {
@@ -11,7 +15,9 @@ class Authentication extends StatefulWidget {
 
 class _AuthenticationState extends State<Authentication> {
 
-  bool newUser = false;
+  bool newUser = true;
+  bool showForm = false;
+  late double _width;
 
   final _key = GlobalKey<FormState>();
 
@@ -22,22 +28,98 @@ class _AuthenticationState extends State<Authentication> {
   TextEditingController password = TextEditingController();
   TextEditingController churchName = TextEditingController();
 
-  double scaleValue = 1;
+  @override
+  void initState() {
+
+    _width = 300;
+
+   Timer(const Duration(milliseconds: 2000), () {
+      setState(() {
+        showForm = true;
+        _width = 0;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
             Expanded(
+              flex: 3,
+              child: Container(
+              
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 199, 180, 12),
+                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20))
+                )
+                
+              )
+            ),
+            const SizedBox(height: 20,),
+            Expanded(
               flex: 6,
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: register(),
-              )
-                  
+                child: AnimatedCrossFade(
+                  excludeBottomFocus: false,
+                  firstChild: Container(
+                    height: MediaQuery.of(context).size.height,
+                    width:  _width,
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(20)
+                    ),
+                    child: (showForm) ? null : const Align(alignment: Alignment.center,child: Text("WELCOME", style: TextStyle(fontSize: 30))),
+                    
+                  ), 
+                  secondChild: register(), 
+                  crossFadeState: (!showForm) ? CrossFadeState.showFirst : CrossFadeState.showSecond, 
+                  duration: const Duration(milliseconds: 2000),
+                )
+              )     
             ),
+
+            Expanded(
+              flex: 1,
+              child: (newUser) ? 
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text("Already have an account? ", style: TextStyle(fontSize: 20),),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        newUser = !newUser;
+                      });
+                    }, 
+                    child: const Text("Login", style: TextStyle(fontSize: 20))
+                  ),
+                          
+                ],
+              ) : 
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text("New user? ", style: TextStyle(fontSize: 20),),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        newUser = !newUser;
+                      });
+                    }, 
+                    child: const Text("Register", style: TextStyle(fontSize: 20))
+                  ),
+            
+                ],
+              )
+            )
         
           ],
         ),
@@ -50,97 +132,113 @@ class _AuthenticationState extends State<Authentication> {
 
     return Form(
       key: _key,
-      child: Column(
-        children: [
-          TextFormField(
-            controller: firstName,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return "Please enter first name";
-              }
-            },
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: "Maxwell",
-              label: Text("First name")
-            ),
-          ),
-
-          const SizedBox(height: 20,),
-
-          TextFormField(
-            controller: lastName,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return "Please enter last name";
-              }
-            },
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: "Jhonson",
-              label: Text("last name")
-            ),
-          ),
-
-          const SizedBox(height: 20,),
-
-          TextFormField(
-            controller: email,
-            keyboardType: TextInputType.emailAddress,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return "Please enter email";
-              } else if (!EmailValidator.validate(value)) {
-                return "Please enter valid email";
-              }
-            },
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: "church@gmail.com",
-              label: Text("Email")
-            ),
-          ),
-
-          const SizedBox(height: 20,),
-
-          DropdownButtonFormField(
-            value: "1",
-            decoration: const InputDecoration(
-              border: OutlineInputBorder()
-            ),
-            icon: const Icon(Icons.keyboard_arrow_down),
-            items: const [
-              DropdownMenuItem(value: "1", child: Text("Member")),
-              DropdownMenuItem(value: "2", child: Text("Church Admin"))
-            ], 
-            onChanged: (value) {
-              setState(() {
-                isMember.text = value!;
-              });
-              
-            }
-          ),
-
-          const SizedBox(height: 20,),
-
-          if (isMember.text == "2") ... [
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
             TextFormField(
-                controller: churchName,
-                keyboardType: TextInputType.text,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Please enter email";
-                  }
-                },
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: "name of church",
-                  label: Text("Church name"),
-                  
-                ),
-              )
-          ]
-        ],
+              controller: firstName,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Please enter first name";
+                }
+              },
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Maxwell",
+                label: Text("First name")
+              ),
+            ),
+        
+            const SizedBox(height: 20,),
+        
+            TextFormField(
+              controller: lastName,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Please enter last name";
+                }
+              },
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Jhonson",
+                label: Text("last name")
+              ),
+            ),
+        
+            const SizedBox(height: 20,),
+        
+            TextFormField(
+              controller: email,
+              keyboardType: TextInputType.emailAddress,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Please enter email";
+                } else if (!EmailValidator.validate(value)) {
+                  return "Please enter valid email";
+                }
+              },
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "church@gmail.com",
+                label: Text("Email")
+              ),
+            ),
+        
+            const SizedBox(height: 20,),
+        
+            DropdownButtonFormField(
+              value: "1",
+              decoration: const InputDecoration(
+                border: OutlineInputBorder()
+              ),
+              icon: const Icon(Icons.keyboard_arrow_down),
+              items: const [
+                DropdownMenuItem(value: "1", child: Text("Member")),
+                DropdownMenuItem(value: "2", child: Text("Church Admin"))
+              ], 
+              onChanged: (value) {
+                setState(() {
+                  isMember.text = value!;
+                });
+                
+              }
+            ),
+        
+            if (isMember.text == "2") ... [
+              const SizedBox(height: 20,),
+        
+              TextFormField(
+                  controller: churchName,
+                  keyboardType: TextInputType.text,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please enter email";
+                    }
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "name of church",
+                    label: Text("Church name"),
+                    
+                  ),
+                )
+            ],
+        
+            const SizedBox(height: 20,),
+        
+            SizedBox(
+              height: 60,
+              width: 150,
+              child: ElevatedButton(
+                onPressed: () { 
+        
+                }, 
+                child: const Text("Register", style: TextStyle(fontSize: 20),)),
+            ),
+        
+            const SizedBox(height: 10,)
+          ],
+        ),
       ) 
     );
   }
